0f40a3c4c14fe4a9564d04903d54fe6cab33926f
DH <dahui.plus@outlook.com>
2024-07-26 11:45:04 +0800

    feat: add task list describe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,6 +10,22 @@ import SubmissionChart from "../components/SubmissionChart.vue"
       <p>You need to submit your completed work to this warehouse, and when you complete the specified task, you will receive the target reward.</p>
       <p>This repository will show the completion status of your tasks, so that you can clearly understand what you have done.</p>
 
+      <h2>Tasks:</h2>
+      <ol class="task-list">
+        <li class="task-list-item">Advance Math: 10 pages / day</li>
+        <li class="task-list-item">Probability theory: 4 pages / day</li>
+        <li class="task-list-item">Linear algebra: 5 pages / day</li>
+        <li class="task-list-item">1000 Questions finish after listening the course: 1 course / day</li>
+        <li class="task-list-item">660 finish after listening the course: 1 course / day</li>
+        <li class="task-list-item">Exercise book after listening the course: 10pages / day</li>
+        <li class="task-list-item">Mr Wu Teaching material: course / day</li>
+        <li class="task-list-item">English words: 2 units / day</li>
+        <li class="task-list-item">English reading: 2 pieces / day</li>
+        <li class="task-list-item">Network course: 2 course / day</li>
+        <li class="task-list-item">Network book: 20 pages / day</li>
+        <li class="task-list-item">Software engineering book: 10 pages / day</li>
+      </ol>
+
       <h2>Usage:</h2>
       <p>1. In the first time you should run the following command in your project root directory to get the lastest tasks:</p>
       <pre>
@@ -23,7 +39,7 @@ import SubmissionChart from "../components/SubmissionChart.vue"
         git add .
 
         # submit to the local storage
-        git commit -m "describe about this commit"
+        git commit -m "submit: describe about this commit"
 
         # push to the remote(I will check your commit in the remote)
         git push origin main
@@ -62,6 +78,16 @@ import SubmissionChart from "../components/SubmissionChart.vue"
 
   .describe {
     text-align: left;
+
+    .task-list {
+      padding: 10px 0 10px 35px;
+      background-color: #eee;
+      border-radius: 10px;
+
+      .task-list-item {
+        color: #666;
+      }
+    }
   }
 
   .contribute {
